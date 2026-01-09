942ee50a5ec5caeceb7df6b9e3d35d5d850ff00a
marcelo a magalhaes <marceloamagalhaes@marcelos-MacBook-Pro.local>
2026-01-08 21:34:51 -0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,14 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     jwtSecret: process.env.JWT_SECRET || 'chave-secreta-padrao',
-    // Chaves públicas ou configuráveis via .env
     runtimeConfig: {
       storagePath: process.env.STORAGE_PATH,
-
-    public: {
-      siteName: 'paravyomayoga',
-      // O Nuxt mapeia automaticamente NUXT_PUBLIC_UPLOAD_PATH do .env para cá
-      uploadPath: process.env.NUXT_PUBLIC_UPLOAD_PATH || 'public/images'
+      public: {
+        siteName: 'paravyomayoga',
+        uploadPath: process.env.NUXT_PUBLIC_UPLOAD_PATH || 'public/images'
+      }
     }
   },
-  
   css: [
     'primeicons/primeicons.css' // <--- ESTA LINHA É OBRIGATÓRIA
   ],
